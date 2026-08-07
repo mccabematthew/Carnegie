4176b1df4c9f74f9fd59d38c67dc3a5b13632a18
mccabematthew <matthew.james.mccabe@outlook.com>
2026-08-06 22:56:29 -0500

    unfinished group anagrams, 'very tired i am' - yoda probably

---
+++ group-anagrams/main.go
@@ -0,0 +1,24 @@
+// Package main implements group anagrams leetcode problem
+package main
+
+import (
+	"fmt"
+)
+
+// Given an arr of string strs, group the anagrams together
+// Return answer in any order
+func groupAnagrams(strs []string) [][]string {
+	// find anagrams
+	// build arr of string arrs around anagrams as we find them
+	groups := [][]string{}
+	
+
+
+	return groups
+}
+
+func main() {
+	fmt.Println(groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"))
+	fmt.Println(groupAnagrams([]string{""}))
+	fmt.Println(groupAnagrams([]string{"a"}))
+}
