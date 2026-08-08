e6a131609994e34068bf0dacecfa8b9cbaf285d2
mccabematthew <matthew.james.mccabe@outlook.com>
2026-08-07 22:09:23 -0500

    no chance I finish this tn

---
+++ buy-sell-stock/main.go
@@ -0,0 +1,24 @@
+// Package main implements Best Time to Buy and Sell Stock solution
+package main
+
+import (
+	"fmt"
+)
+
+// Given an arr prices where prices[i] is the price
+// of a given stock on the i'th day
+//	- maximize your profit by choosing a single day to buy
+// 	one stock and choosing in a different day in the future to sell
+//	- return the maximum profit you can achieve from this transaction
+//	If you can't achieve any profit, ret 0
+func maxProfit(prices []int) int {
+	
+	return 0
+}
+
+func main() {
+	fmt.Println(maxProfit([]int{7,1,5,3,6,4})
+	fmt.Println(maxProfit([]int{7,6,4,3,1})
+	fmt.Println(maxProfit([]int{3,7,3,4,1,8})
+}
+
