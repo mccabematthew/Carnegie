9788b4377f712ffc5a9b2f5399c1351bbafa1bc0
mccabematthew <matthew.james.mccabe@outlook.com>
2026-08-08 20:39:42 -0500

    backlog getting bizzy

---
+++ valid-palindrome/main.go
@@ -0,0 +1,17 @@
+// Package main implements LeetCode valid palindrome solution
+package main
+
+import (
+	"fmt"
+)
+
+func validPalindrome(s string) bool {
+	
+}
+
+func main() {
+	fmt.Println(validPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(validPalindrome("race a car"))
+	fmt.Println(validPalindrome(" "))
+}
+
