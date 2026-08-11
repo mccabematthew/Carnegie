4be41194c20b4371f83b328f443583f5ea957b63
mccabematthew <matthew.james.mccabe@outlook.com>
2026-08-11 16:40:29 -0500

    add longest-substring stub

---
+++ longest-substring/main.go
@@ -0,0 +1,16 @@
+// Package main implements solution to LeetCode longest substring without repeating characters
+package main
+
+import (
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+	
+}
+
+func main() {
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+}
