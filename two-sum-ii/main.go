a546e7140d7f3d875d258f24a20fc1903dd7ed0f
mccabematthew <matthew.james.mccabe@outlook.com>
2026-08-10 20:30:29 -0500

    init two-sum-ii

---
+++ two-sum-ii/main.go
@@ -0,0 +1,17 @@
+// Pacakge main implements LeetCode two sum II solution
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(numvers []int, target int) []int {
+
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2,7,11,15}, 9))
+	fmt.Println(twoSum([]int{2,3,4}, 6))
+	fmt.Println(twoSum([]int{-1,0}, -1))
+}
+
