42eb11eb9aa6a80aa6bf1dcd738a9419e79c8be6
mccabematthew <matthew.james.mccabe@outlook.com>
2026-08-12 21:21:07 -0500

    two sum II

--- two-sum-ii/main.go
+++ two-sum-ii/main.go
@@ -1,17 +1,29 @@
-// Pacakge main implements LeetCode two sum II solution
+}// Package main implements LeetCode Two Sum II solution
 package main
 
 import (
 	"fmt"
 )
 
-func twoSum(numvers []int, target int) []int {
+func twoSum(numbers []int, target int) []int {
+	left, right := 0, len(numbers)-1
 
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		switch {
+		case sum == target:
+			return []int{left + 1, right + 1} // LeetCode's Two Sum II wants 1-indexed answers
+		case sum < target:
+			left++
+		default:
+			right--
+		}
+	}
+	return nil
 }
 
 func main() {
-	fmt.Println(twoSum([]int{2,7,11,15}, 9))
-	fmt.Println(twoSum([]int{2,3,4}, 6))
-	fmt.Println(twoSum([]int{-1,0}, -1))
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{2, 3, 4}, 6))
+	fmt.Println(twoSum([]int{-1, 0}, -1))
 }
-
